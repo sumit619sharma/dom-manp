0a6cb431da4c64f0d23be623fa3179014aedd223
unknown <imsumitsharma619@gmail.com>
2023-05-22 11:50:28 +0530

    style- elements through javascript

--- demo.js
+++ demo.js
@@ -1,1 +1,5 @@
-console.log("demo-file is ready to push");
+document.getElementById('main-header').style.borderBottom = 'solid 3px black'
+document.getElementsByClassName('title')[0].style.color = 'green';
+document.getElementsByClassName('title')[0].style.fontweight = 'bold';
+document.getElementsByClassName('title')[1].style.color = 'red';
+document.getElementsByClassName('title')[1].style.fontSize='50px';
