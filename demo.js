8a47d2fca519037f46be286aa88f252bc107704b
unknown <imsumitsharma619@gmail.com>
2023-05-22 12:30:36 +0530

    change style of same class element through for-loop

--- demo.js
+++ demo.js
@@ -3,3 +3,12 @@ document.getElementsByClassName('title')[0].style.color = 'green';
 document.getElementsByClassName('title')[0].style.fontweight = 'bold';
 document.getElementsByClassName('title')[1].style.color = 'red';
 document.getElementsByClassName('title')[1].style.fontSize='50px';
+
+
+document.getElementsByClassName('list-group-item')[2].style.color="green";
+var list = document.getElementsByClassName('list-group-item')
+console.log(list);
+for(let i=0; i<list.length; i++){
+    list[i].style.fontweight="bold";
+    list[i].style.backgroundColor="grey";
+}
